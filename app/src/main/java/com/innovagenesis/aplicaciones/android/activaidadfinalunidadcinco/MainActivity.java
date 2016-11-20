94e0d8c1c8fdab9682013de7a1105992082694ba
AlexisRoj <alexisroj2@gmail.com>
2016-11-19 18:14:50 -0600

    Se carga con menus funcionando y los intent aplicados

--- app/src/main/java/com/innovagenesis/aplicaciones/android/activaidadfinalunidadcinco/MainActivity.java
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/activaidadfinalunidadcinco/MainActivity.java
@@ -1,12 +1,14 @@
 package com.innovagenesis.aplicaciones.android.activaidadfinalunidadcinco;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.SubMenu;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,15 +16,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private String[] etiquetaSubMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -59,12 +67,26 @@ public class MainActivity extends AppCompatActivity
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
 
-        String [] etiquetaMenu = getResources().getStringArray(R.array.nombreMenu);
+        /** Traen las etiquetas e imagenes del subMenu*/
+        etiquetaSubMenu = getResources().getStringArray(R.array.nombreMenu);
+        @SuppressLint("Recycle")
+        TypedArray imgSubMenu = getResources().obtainTypedArray(R.array.nombreImgMenu);
+        String[] uriIntent = getResources().getStringArray(R.array.uriMenu);
 
+        Intent intent;
 
         SubMenu versionWeb = menu.addSubMenu(R.string.version_web);
 
+        for (int i = 0; i < etiquetaSubMenu.length - 1; i++) {
+            /** Agrega submenu, se envia uri a traves de un arreglo
+             * Se le suma uno a i para reutilizar arreglo de etiquetas*/
 
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uriIntent[i + 1]));
+
+            versionWeb.add(0, i + 1, i + 1, etiquetaSubMenu[i + 1])
+                    .setIcon(imgSubMenu.getResourceId(i + 1, 0))
+                    .setIntent(intent);
+        }
         return true;
     }
 
@@ -95,20 +117,31 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        int positionArray = 0;
 
-        switch (id){
+
+        switch (id) {
             /** Selector de fragmentos del Drawer*/
             case R.id.nav_home:
+                positionArray = 0;
                 break;
             case R.id.nav_facebook:
+                positionArray = 1;
                 break;
             case R.id.nav_instagram:
+                positionArray = 2;
                 break;
             case R.id.nav_plus:
+                positionArray = 3;
                 break;
             case R.id.nav_tweeter:
+                positionArray = 4;
                 break;
         }
+
+        /** Cambia el titulo, subtitulo y color */
+        toolbar.setTitle(getString(R.string.app_name));
+        toolbar.setSubtitle(etiquetaSubMenu[positionArray]);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
